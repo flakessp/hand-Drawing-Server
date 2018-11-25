909e6bcc16f6844c31d90181cfb8f2387ec7ab17
flakessp <flakessp@gmail.com>
2018-11-25 23:36:23 +0300

    👀

---
+++ README.md
@@ -0,0 +1,17 @@
+# hand-Drawing-Server
+simple node.js + express res/req ndjson parser backend for google drawing
+
+## Build Setup
+
+``` bash
+# install dependencies
+npm install
+
+# serve with hot reload at localhost:8080
+npm run dev
+
+# build for production with minification
+npm run build
+```
+
+For detailed explanation on how things work, consult the [docs for vue-loader](http://vuejs.github.io/vue-loader).
---
+++ src/components/NewQuote.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="row">
+        <form>
+            <div class="col-sm-8 col-sm-offset-2 col-xs-12 col-md-6 col-md-offset-3 form-group">
+               <button class="btn btn-primary" @click.prevent="createNew">{{quote}}</button>
+            </div>
+        </form>
+    </div>
+</template>
+
+<script>
+    export default {
+        data: function() {
+            return {
+                quote: 0
+            };
+        },
+        methods: {
+            createNew() {
+                this.$emit('quoteAdded', this.quote);
+                this.quote ++;
+            }
+        }
+    }
+</script>
+
+<style>
+
+</style>
